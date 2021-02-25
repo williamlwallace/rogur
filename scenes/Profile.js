fa12fe9af6c196a169ac9967d4db43a0a6f3fc4a
William Wallace <wwa52@uclive.ac.nz>
2021-02-25 23:16:10 +1300

    added profile screen

---
+++ scenes/Profile.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { StyleSheet, View } from 'react-native';
+import { render } from 'react-dom';
+
+const Profile = () => {
+
+    return (
+        <View></View>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+
+    }
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default connect(MapStateToProps, MapDispatchToProps)(Profile);
